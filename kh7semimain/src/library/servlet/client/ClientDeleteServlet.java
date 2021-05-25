d5d4f9109251485ea77c844204258a9d99e3ef4a
user1 <user1@DESKTOP-AC3QINP>
2021-05-25 15:51:44 +0900

    회원정보 수정, 삭제, 목록 관리자 입장에서

---
+++ kh7semimain/src/library/servlet/client/ClientDeleteServlet.java
@@ -0,0 +1,31 @@
+package library.servlet.client;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import library.beans.ClientDao;
+
+@WebServlet(urlPatterns = "/client/clientDelete.kh")
+public class ClientDeleteServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+
+			int clientNo = Integer.parseInt(req.getParameter("clientNo"));
+			
+			ClientDao clientDao = new ClientDao();
+			clientDao.delete(clientNo);
+			
+			resp.sendRedirect("clientList.jsp");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}

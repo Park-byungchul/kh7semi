6c52aa4e3149fb835438152c5142244e4aa2f789
Ha Jeongyeon <hjy---@hanmail.net>
2021-05-25 10:46:57 +0900

    게시글, 리뷰, 댓글 DTO + SQL 오타 수정

---
+++ kh7semimain/src/library/beans/ReviewCommentDto.java
@@ -0,0 +1,49 @@
+package library.beans;
+
+import java.sql.Date;
+
+public class ReviewCommentDto {
+	int commentNo;
+	int clientNo;
+	int reviewNo;
+	String commentField;
+	Date commentDate;
+	int commentLike;
+	
+	public int getCommentNo() {
+		return commentNo;
+	}
+	public void setCommentNo(int commentNo) {
+		this.commentNo = commentNo;
+	}
+	public int getClientNo() {
+		return clientNo;
+	}
+	public void setClientNo(int clientNo) {
+		this.clientNo = clientNo;
+	}
+	public int getReviewNo() {
+		return reviewNo;
+	}
+	public void setReviewNo(int reviewNo) {
+		this.reviewNo = reviewNo;
+	}
+	public String getCommentField() {
+		return commentField;
+	}
+	public void setCommentField(String commentField) {
+		this.commentField = commentField;
+	}
+	public Date getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(Date commentDate) {
+		this.commentDate = commentDate;
+	}
+	public int getCommentLike() {
+		return commentLike;
+	}
+	public void setCommentLike(int commentLike) {
+		this.commentLike = commentLike;
+	}
+}

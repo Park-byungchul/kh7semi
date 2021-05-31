157b703c342c77078b9316e6e711cecb488a1b1b
yongbo Sim <dydqh2648@naver.com>
2021-05-31 16:55:22 +0900

    권한 CRUD 구현

---
+++ kh7semimain/src/library/servlet/role/RoleEditServlet.java
@@ -0,0 +1,38 @@
+package library.servlet.role;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import library.beans.RoleDao;
+import library.beans.RoleDto;
+
+@WebServlet(urlPatterns = "/role/roleEdit.kh")
+public class RoleEditServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		try {
+			req.setCharacterEncoding("UTF-8");
+			int roleClientNo = Integer.parseInt(req.getParameter("roleClientNo"));
+			int roleAreaNo = Integer.parseInt(req.getParameter("roleAreaNo"));
+
+			RoleDto roleDto = new RoleDto();
+			roleDto.setClientNo(Integer.parseInt(req.getParameter("clientNo")));
+			roleDto.setAreaNo(Integer.parseInt(req.getParameter("areaNo")));
+			
+			RoleDao roleDao = new RoleDao();
+			roleDao.edit(roleDto, roleClientNo, roleAreaNo);
+			
+			resp.sendRedirect("roleList.jsp");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			resp.sendError(500);
+		}
+	}
+}

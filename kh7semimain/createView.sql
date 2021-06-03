5e7423fa3812ec764f2974038b69517f65f33de2
Ha Jeongyeon <hjy---@hanmail.net>
2021-06-04 01:26:09 +0900

    리뷰 게시판 좋아요, 댓글 기능 구현
    createTable에 있는 alter랑 게시판 insert 해주세요!
    기존에 있는걸로 합치려고 노력하다가 그냥 새로 Dao Dto 서블릿 다 팠어요..

--- kh7semimain/createView.sql
+++ kh7semimain/createView.sql
@@ -61,7 +61,9 @@ where B.board_type_no = 2;
 --리뷰 목록 출력을 위한 view
 create view review_list as
 select R.review_no, R.client_no as reviewer, R.book_isbn, R.review_subject,
-        R.review_content, R.review_read, R.review_like, R.review_date,
+        R.review_content, R.review_read, R.review_like, R.review_date, R.review_reply,
         C.client_no, C.client_name
 from review R
 left outer join client C on R.client_no = C.client_no;
+
+drop view review_list;

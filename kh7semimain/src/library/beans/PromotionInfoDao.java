d9c068bbbae366c3289650322ea5ff1615d466d7
yongbo Sim <dydqh2648@naver.com>
2021-06-05 18:45:09 +0900

    메인페이지 배너

--- kh7semimain/src/library/beans/PromotionInfoDao.java
+++ kh7semimain/src/library/beans/PromotionInfoDao.java
@@ -33,4 +33,94 @@ public class PromotionInfoDao {
 		con.close();
 		return list;
 	}
+	
+	public PromotionInfoDto detail(int promotionPage) throws Exception {
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from"
+				+ "(select rownum rn, TMP.* from"
+				+ "(select * from promotion P left outer join promotion_file F on P.promotion_no = F.file_origin order by promotion_no asc)"
+				+ "TMP where file_no is not null)"
+				+ "where rn = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, promotionPage);
+		ResultSet rs = ps.executeQuery();
+		
+		PromotionInfoDto promotionInfoDto = new PromotionInfoDto();
+		
+		if(rs.next()) {
+			promotionInfoDto.setPromotionNo(rs.getInt("promotion_no"));
+			promotionInfoDto.setAreaNo(rs.getInt("area_no"));
+			promotionInfoDto.setFileNo(rs.getInt("file_no"));
+			promotionInfoDto.setFileUploadName(rs.getString("file_upload_name"));
+			promotionInfoDto.setFileSaveName(rs.getString("file_save_name"));
+			promotionInfoDto.setFileContentType(rs.getString("file_content_type"));
+			promotionInfoDto.setFileSize(rs.getLong("file_size"));
+			promotionInfoDto.setFileOrigin(rs.getInt("file_origin"));
+		} else {
+			promotionInfoDto = null;
+		}
+		
+		con.close();
+		return promotionInfoDto;
+	}
+	
+	public PromotionInfoDto detail(int promotionPage, int areaNo) throws Exception {
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select * from"
+				+ "(select rownum rn, TMP.* from"
+				+ "(select * from promotion P left outer join promotion_file F on P.promotion_no = F.file_origin order by promotion_no asc)"
+				+ "TMP where file_no is not null and (area_no = ? or area_no is null))"
+				+ "where  rn = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, areaNo);
+		ps.setInt(2, promotionPage);
+		ResultSet rs = ps.executeQuery();
+		
+		PromotionInfoDto promotionInfoDto = new PromotionInfoDto();
+		
+		if(rs.next()) {
+			promotionInfoDto.setPromotionNo(rs.getInt("promotion_no"));
+			promotionInfoDto.setAreaNo(rs.getInt("area_no"));
+			promotionInfoDto.setFileNo(rs.getInt("file_no"));
+			promotionInfoDto.setFileUploadName(rs.getString("file_upload_name"));
+			promotionInfoDto.setFileSaveName(rs.getString("file_save_name"));
+			promotionInfoDto.setFileContentType(rs.getString("file_content_type"));
+			promotionInfoDto.setFileSize(rs.getLong("file_size"));
+			promotionInfoDto.setFileOrigin(rs.getInt("file_origin"));
+		} else {
+			promotionInfoDto = null;
+		}
+		
+		con.close();
+		return promotionInfoDto;
+	}
+	
+	public int count() throws Exception {
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select count(*) from promotion P left outer join promotion_file F on P.promotion_no = F.file_origin where file_no is not null";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+		
+		con.close();
+		return count;
+	}
+	
+	public int count(int areaNo) throws Exception {
+		Connection con = JdbcUtils.getConnection();
+		
+		String sql = "select count(*) from promotion P left outer join promotion_file F on P.promotion_no = F.file_origin where file_no is not null and (area_no = ? or area_no is null)";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, areaNo);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		int count = rs.getInt(1);
+		
+		con.close();
+		return count;
+	}
 }
